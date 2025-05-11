ed9cf83ca13ae6354bf3e7edbc22eea18f2b955d
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-11 10:17:39 -0700

    Completed Section 3

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 challenges = {
@@ -20,8 +21,13 @@ challenges = {
 
 def monthly_challenge_by_number(request, month):
     months = list(challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse(
+        "monthly-challenge", args=[redirect_month])  # /challenge/january
+    return HttpResponseRedirect(redirect_path)
 
 
 def handle_month(month):
@@ -32,4 +38,15 @@ def handle_month(month):
 
 
 def monthly_challenge(request, month):
-    return HttpResponse(handle_month(month))
+    challenge_text = handle_month(month)
+    response_data = f"<h1>{challenge_text}</h1>"
+    return HttpResponse(response_data)
+
+
+def month_list(request):
+    month_list = ""
+    for month in list(challenges.keys()):
+        month_path = reverse("monthly-challenge", args=[month])
+        month_list += f'<li><a href="{month_path}">{month.capitalize()}</a></li>'
+    response_data = f"<h1>List</h1><ul>{month_list}</ul>"
+    return HttpResponse(response_data)
